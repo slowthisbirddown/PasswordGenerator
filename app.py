7bd1be0cfe730de28cc51517412099053d614954
slowthisbirddown <hupo1989@gmail.com>
2020-10-16 12:10:40 -0600

    More flash message

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-##
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect
 import PasswordGenerator
 
 app = Flask(__name__)
+app.secret_key = "380n70f4"
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        try:
+            length = int(request.form['length'])
+            pg = PasswordGenerator.PasswordGenerator()
+            password = pg.generate_password(length)   
+            flash(password)
+            return redirect("/")
+        except: 
+            # TODO Flask Flash an error message
+            return "Length is not an integer."
+    else:
+        return render_template('index.html')          
 
 @app.route('/<int:length>')    
 def generate_password(length):
